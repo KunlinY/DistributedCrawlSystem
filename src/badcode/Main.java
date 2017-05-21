1bc669a44f94df85cc75dd14d1469543e404d405
KunlinY <KunlinY@qq.com>
2017-05-21 12:58:46 +0800

    成功与PostgreSQL建立连接

--- src/badcode/Main.java
+++ src/badcode/Main.java
@@ -1,15 +1,9 @@
 package badcode;
 
 
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-
 public class Main {
 
     public static void main(String[] args)  throws Exception {
-
+        NLP nlp = new NLP();
     }
 }
--- src/badcode/NLP.java
+++ src/badcode/NLP.java
@@ -1,11 +1,34 @@
 package badcode;
 
+import org.jooq.*;
+import org.jooq.impl.DSL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.ArrayList;
 import java.util.HashSet;
 
 public class NLP {
-    NLP() {
+    private static String user = "postgres";
+    private static String password = "java";
+    private static String url = "jdbc:postgresql://123.206.72.211:5432/postgres";
+    private static Connection connection = null;
 
+    NLP() {
+        if (connection == null) {
+            try {
+                Class.forName("org.postgresql.Driver").newInstance();
+                connection = DriverManager.getConnection(url, user, password);
+                DSLContext dslContext = DSL.using(connection, SQLDialect.POSTGRES_9_3);
+                Table<Record> table = DSL.table("test");
+
+                Result<Record> result = dslContext.select().from(table).fetch();
+                System.out.println(result.size());
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Cannot connect to NLP SQL!");
+            }
+        }
     }
 
     public class News {
